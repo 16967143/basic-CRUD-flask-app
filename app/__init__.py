5c49c12ea06a5bee279a0d88441ec971e789a2e2
davistodt <davis.todt@roche.com>
2018-02-18 10:52:33 +0200

    initial setup complete up to and including first migration

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+# app/__init__.py
+
+# third-party imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
+# local imports
+from config import app_config
+
+# db variable initialization
+db = SQLAlchemy()
+# login manager initialisation
+login_manager = LoginManager()
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+    
+    from app import models
+
+    return app
