c437ca701e266edf8820a3b6aeff8a6b24799249
davistodt <davis.todt@roche.com>
2018-02-18 16:18:57 +0200

    add login+reg forms and views and dashboard view

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 # local imports
 from config import app_config
@@ -18,12 +19,12 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    db.init_app(app)
 
+    Bootstrap(app)
+    db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
-
     migrate = Migrate(app, db)
 
     from app import models
