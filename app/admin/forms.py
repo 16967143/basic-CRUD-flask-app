b9ed0e96117b80d3c60e2ff8b488363a079ba3a4
davistodt <davis.todt@roche.com>
2018-02-19 16:10:13 +0200

    add Roles functionality and view

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -11,3 +11,11 @@ class DepartmentForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class RoleForm(FlaskForm):
+    """
+    Form for admin to add or edit a role
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
